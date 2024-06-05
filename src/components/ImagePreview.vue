5ff3e265c5f0a31288a41de271dc53db2980e5fb
Camila Serra <66450896+camilaserra@users.noreply.github.com>
2024-06-05 18:14:39 -0300

    refactor: Improve image preview component styles for responsiveness

--- src/components/ImagePreview.vue
+++ src/components/ImagePreview.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="image-preview-wrapper">
     <p>
       Aguardando conteúdo. Informe os canais e as mídias desejadas para
       visualização.
@@ -8,22 +8,30 @@
   </div>
 </template>
 
-<style scoped>
-p {
-  margin: 0px;
-  max-width: 100%;
-  width: 500px;
-  font-size: 20px;
-  padding-bottom: 20px;
-  padding-top: 20px;
+<style lang="scss" scoped>
+.image-preview-wrapper {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  p {
+    margin: 0px;
+    max-width: 100%;
+    width: 500px;
+    font-size: 20px;
+    padding-bottom: 20px;
+    padding-top: 20px;
 
-  @media (max-width: 1020px) {
-    font-size: 16px;
-    width: 100%;
+    @media (max-width: 1020px) {
+      font-size: 16px;
+      width: 100%;
+    }
   }
-}
 
-img {
-  height: 300px;
+  img {
+    height: 300px;
+  }
 }
 </style>
